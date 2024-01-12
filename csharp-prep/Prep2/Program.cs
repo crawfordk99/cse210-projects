bb54d35563203d50332987886d899e91413fefcc
Keith Crawford <keithacrawfordjr@gmail.com>
2024-01-12 11:52:44 -0700

    Did prep activities.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string input= Console.ReadLine();
+        int perc= int.Parse(input);
+        string letter= "";
+        if(perc >= 90){
+            letter = "A";
+        }
+        else if (perc >= 80) {
+            letter = "B";
+        }
+        else if (perc >= 70) {
+            letter = "C";
+        }
+        else if (perc >=60) {
+            letter = "D";
+        }
+        else {
+            letter = "F";
+        }
+        
+        if (perc >=70) {
+            Console.WriteLine("You passed!");
+        }
+        Console.WriteLine($"{letter}");
     }
 }
