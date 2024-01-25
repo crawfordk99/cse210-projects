6a9edce8204ff24119f707d3e72f6f7b7547e938
Keith Crawford <keithacrawfordjr@gmail.com>
2024-01-24 21:38:10 -0700

    Finished Learning02 project

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1= new Job();
+        job1._company= "360 Logistics LLC";
+        job1._jobTitle= "Delivery Driver";
+        job1._startYear= 2023;
+        job1._endYear= 2023;
+        // job1.Display();
+
+        Job job2= new Job();
+        job2._company= "Ironwood Insights Group";
+        job2._jobTitle= "Interviewer";
+        job2._startYear= 2023;   
+        job2._endYear= 2023;
+        // job2.Display();
+
+        Resume myResume= new Resume();
+        myResume._name= "Keith Crawford";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        // Console.WriteLine(myResume._jobs[0]._jobTitle);
+        myResume.DisplayResume();
     }
 }
