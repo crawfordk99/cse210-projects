fd679daf282f9a7062dfbde9ca34bb061c3d4778
Keith Crawford <keithacrawfordjr@gmail.com>
2024-03-05 17:54:10 -0700

    Added a list and foreach loop to main

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -7,12 +7,13 @@ class Program
        Square square = new Square("Blue", 3);
        Rectangle rectangle = new Rectangle("Orange", 4, 4.5);
        Circle circle= new Circle("Red", 3.5);
-       Console.WriteLine(square.GetColor());
-       Console.WriteLine(square.GetArea());
-       Console.WriteLine(rectangle.GetColor());
-       Console.WriteLine(rectangle.GetArea());
-       Console.WriteLine(circle.GetColor());
-       Console.WriteLine(circle.GetArea());
-       
+       List<Shape> _shapes = new List<Shape>();
+       _shapes.Add(square);
+       _shapes.Add(rectangle);
+       _shapes.Add(circle); 
+       foreach (Shape shape in _shapes) {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+       }
     }
 }
